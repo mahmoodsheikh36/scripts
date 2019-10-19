1c9997e493f2fa4709bf7b00a7afbd31e6473c46
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2019-10-19 21:36:13 +0300

    improve script

--- backgrounder.sh
+++ backgrounder.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
+
 # backgrounder.sh - fade transition between wallpapers
 # use like this: ./backgrounder.sh ~/wallpaper
+# this script is dirty af and i doubt its anywhere close to being efficient
 
 echo using pictures in directory $1
 cd $1
 image_dir="$1"
-images=$(find . -name "*.jpg" -or -name "*.jpeg" | shuf)
+images=$(find . -path ./ignore -prune -o -name "*.jpg" -print -o -name "*.jpeg" -print -o -name "*.png" -print | shuf)
 image_cache_dir=~/.cache/backgrounder
-dimmed_images_count=5
+dimmed_images_count=10
 image_count=$(echo $images | wc -w)
+screen_size=`xrandr | grep primary | cut -d " " -f4 | cut -d "+" -f1`
+image_time=200 # display each image for 200 seconds
+
+if [ ! -z "$2" ]; then
+    image_time="$2"
+fi
 
 # create cache directory
 if [ ! -d "$image_cache_dir" ]; then
@@ -16,45 +24,48 @@ if [ ! -d "$image_cache_dir" ]; then
 fi
 
 generate_dimmed_pictures() {
-    for ((i=1; i<=$image_count; i++))
+    for ((d=1; d<=$image_count; d++))
     do
         dim_percentage=0%
-        image=$(basename `echo $images | cut -d ' ' -f "$i"`)
-        for ((j=0; j<=$dimmed_images_count; j++))
+        image=$(basename `echo $images | cut -d ' ' -f "$d"`)
+        generated_new_images=0
+        for ((f=0; f<=$dimmed_images_count; f++))
         do
-            dim_percentage="`expr 100 - $j \* $(expr 100 / $dimmed_images_count)`%"
+            dim_percentage="`expr 100 - $f \* $(expr 100 / $dimmed_images_count)`%"
             file_to_save_dimmed_image="${image_cache_dir}/$image-$dim_percentage"
             if [ ! -f $file_to_save_dimmed_image ]; then
-                convert -fill black -colorize "$dim_percentage" "$image" "$file_to_save_dimmed_image"
+                generated_new_images=1
+                convert -fill black -resize "$screen_size!" -colorize "$dim_percentage" "$image" "$file_to_save_dimmed_image"
             fi
         done
-        notify.sh "generated dimmed images for $image"
+        if [ $generated_new_images = 1 ]; then
+            notify-send -i "${image_dir}/$image" "System Wallpaper Manager" "generated dimmed images for\n$image"
+        fi
     done
-    notify.sh "done generating dimmed images"
+    notify-send "System Wallpaper Manager" "done generating dimmed images" -t 10
 }
 
 while [ true ]
 do
     for ((i=1; i<=$image_count; i++))
     do
-        image=$(echo $images | cut -d ' ' -f "$i")
-        echo "require('naughty').notify({title='System Wallpaper', text='set wallpaper to\n$image', icon='${image_dir}/$image', icon_size=200})" | awesome-client
+        image=`basename $(echo $images | cut -d ' ' -f "$i")`
 
         dim_percentage=100%
         for ((j=0; j<=$dimmed_images_count; j++))
         do
             image_file="${image_cache_dir}/$image-$dim_percentage"
             if [ ! -f $image_file ]; then
-                notify.sh "regenerating dimmed images"
+                notify-send "System Wallpaper Manager" "regenerating dimmed images"
                 generate_dimmed_pictures
             fi
-            hsetroot -fill "$image_file"
+            hsetroot -fill "$image_file" > /dev/null
             dim_percentage="`expr 100 - $(expr $j + 1) \* $(expr 100 / $dimmed_images_count)`%"
-            echo $image_file
             sleep 0.01
         done
+        notify-send -i "${image_dir}/$image" "System Wallpaper Manager" "set wallpaper to\n$image" -t 3
 
-        sleep 200 # display each image for 200 seconds
+        sleep $image_time
 
         dim_percentage=0%
         image=$(echo $images | cut -d ' ' -f "$i")
@@ -62,12 +73,11 @@ do
         do
             dim_percentage="`expr $(expr $j + 1) \* $(expr 100 / $dimmed_images_count)`%"
             image_file="${image_cache_dir}/$image-$dim_percentage"
-            echo $image_file
             if [ ! -f $image_file ]; then
-                notify.sh "regenerating dimmed images"
+                notify-esnd "System Wallpaper Manager" "regenerating dimmed images"
                 generate_dimmed_pictures
             fi
-            hsetroot -fill "$image_file"
+            hsetroot -fill "$image_file" > /dev/null
             sleep 0.01
         done
 
