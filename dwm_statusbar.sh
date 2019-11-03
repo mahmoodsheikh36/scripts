6fed602c6a41d0936cec303309f848e8ba81240a
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2019-11-03 18:46:40 +0200

    moved back to arch

--- dwm_statusbar.sh
+++ dwm_statusbar.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
+
+
 while true;
 do
-    xsetroot -name "`dwm_statusbar_text.sh`"
+    time=`date "+%H:%M:%S"`
+    statusbar_text="$time"
+    # volume=`amixer get Speaker -c 1 | tail -1 | cut -d "[" -f2 | cut -d "%" -f1`
+
+    xsetroot -name "$statusbar_text"
     sleep 0.5
 done
