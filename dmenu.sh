638707808b46736e7386f7cacf9df3f2ca567aee
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2019-12-01 15:23:33 +0200

    dont load pywal colors if they dont exist

--- dmenu.sh
+++ dmenu.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-
-# Import the colors
-. "${HOME}/.cache/wal/colors.sh"
-
-dmenu -nb "$color0" -nf "$color15" -sb "$color1" -sf "$color15" "$@" < /dev/stdin
+colors_file="${HOME}/.cache/wal/colors.sh"
+if [ -f "$colors_file" ]; then
+	. "$colors_file"
+	dmenu -nb "$color0" -nf "$color15" -sb "$color1" -sf "$color15" "$@" < /dev/stdin
+else
+	dmenu "$@" < /dev/stdin
+fi
