7b8577dfed9d0b4746cb99ba6997915288cf54ba
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-02-23 01:13:10 +0200

    add year,cue_file_path,log_file_path options

--- website_scripts/add_album.sh
+++ website_scripts/add_album.sh
@@ -17,9 +17,17 @@ if [ -z "$3" ]; then
     exit 1
 fi
 
+if [ -z "$4" ]; then
+    echo enter the year of the album as fourth argument
+    exit 1
+fi
+
 album_name="$1"
 artist_id="$2"
 image_file_path="$3"
+year="$4"
+cue_file_path="$5"
+log_file_path="$6"
 
 urlencode () {
     python3 -c "import urllib.parse, sys; print(urllib.parse.quote(sys.stdin.read()))"
@@ -27,3 +35,25 @@ urlencode () {
 
 curl -X POST "$BACKEND?album_name=$(echo -n "$album_name" | urlencode)&artist_id=$artist_id" \
     -F "image=@$image_file_path" -F 'username=mahmooz' -F 'password=mahmooz' | jq
+
+if [ ! -z "$cue_file_path" ]; then
+    if [ ! -z "$log_file_path" ]; then
+        curl -X POST "$BACKEND?album_name=$(echo -n "$album_name" | urlencode)&artist_id=$artist_id&year=$year" \
+            -F "image=@$image_file_path" -F "cue=@$cue_file_path" -F "log=@$log_file_path" \
+            -F 'username=mahmooz' -F 'password=mahmooz' | jq
+    else
+        curl -X POST "$BACKEND?album_name=$(echo -n "$album_name" | urlencode)&artist_id=$artist_id&year=$year" \
+            -F "image=@$image_file_path" -F "cue=@$cue_file_path" \
+            -F 'username=mahmooz' -F 'password=mahmooz' | jq
+    fi
+else
+    if [ ! -z "$log_file_path" ]; then
+        curl -X POST "$BACKEND?album_name=$(echo -n "$album_name" | urlencode)&artist_id=$artist_id&year=$year" \
+            -F "image=@$image_file_path" -F "log=@$log_file_path" \
+            -F 'username=mahmooz' -F 'password=mahmooz' | jq
+    else
+        curl -X POST "$BACKEND?album_name=$(echo -n "$album_name" | urlencode)&artist_id=$artist_id&year=$year" \
+            -F "image=@$image_file_path" \
+            -F 'username=mahmooz' -F 'password=mahmooz' | jq
+    fi
+fi
