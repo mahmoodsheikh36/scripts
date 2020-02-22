1ae31c5ec9fc07da80529fe356912a07014514c9
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-02-23 01:12:27 +0200

    make it multithreaded

--- view_audio_spectrum.sh
+++ view_audio_spectrum.sh
@@ -2,5 +2,5 @@
 # show audio spectrum using sox
 
 random_str=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13)
-sox "$1" -n spectrogram -o /tmp/$random_str.png && sxiv /tmp/$random_str.png
+sox --multi-threaded "$1" -n spectrogram -o /tmp/$random_str.png && sxiv /tmp/$random_str.png
 rm /tmp/$random_str.png
