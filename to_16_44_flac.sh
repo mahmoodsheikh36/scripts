ff4f3f454efe85969e5c58285fa9811f43f0352e
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-03-24 04:08:07 +0200

    change frequency to 44.1k, was 48l

--- to_16_44_flac.sh
+++ to_16_44_flac.sh
@@ -3,5 +3,5 @@
 for f in *.flac;
 do
     echo "Processing $f"
-    ffmpeg -i "$f" -sample_fmt s16 -ar 48000 "${f%.flac}-16.flac"
+    ffmpeg -i "$f" -sample_fmt s16 -ar 44100 "${f%.flac}-16.flac"
 done
