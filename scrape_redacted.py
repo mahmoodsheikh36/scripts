072da675ad8d95c7835ae6eb4e5d182c347ba686
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-03-25 04:38:53 +0200

    import ThreadPoolExecutor, forgot to do that last commit

--- scrape_redacted.py
+++ scrape_redacted.py
@@ -2,6 +2,7 @@
 import requests
 import numpy
 import random
+from concurrent.futures import ThreadPoolExecutor
 
 headers = {
     'user-agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:74.0) Gecko/20100101 Firefox/74.0'
