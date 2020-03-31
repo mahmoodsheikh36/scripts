892dc6d722ef3132d1c0850de32078a0fc149145
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-03-31 21:59:01 +0300

    accept artist_name, song_name from cmdline args, exit cleanly when no lyrics found

--- get_genius_lyrics.py
+++ get_genius_lyrics.py
@@ -2,15 +2,17 @@
 # get lyrics from genius.com
 import requests
 from bs4 import BeautifulSoup
+import sys
 
-artist_name = 'radiohead'
-song_name   = 'everything in its right place'
+artist_name = sys.argv[2].replace(' ', '-').capitalize()
+song_name   = sys.argv[1].replace(' ', '-')
  
 def get_lyrics_tag(artist_name, song_name):
-    url = "https://genius.com/{}-{}-lyrics".format(
-            '-'.join(artist_name.capitalize().split(' ')),
-            '-'.join(song_name.split(' ')))
+    url = "https://genius.com/{}-{}-lyrics".format(artist_name, song_name)
     soup = BeautifulSoup(requests.get(url).content.decode(), 'html.parser')
     return soup.find_all("div", {"class": "lyrics"})[0]
 
-print(get_lyrics_tag(artist_name, song_name).find_all('p')[0].text)
+try:
+    print(get_lyrics_tag(artist_name, song_name).find_all('p')[0].text)
+except:
+    pass
