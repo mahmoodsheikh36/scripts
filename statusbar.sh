8e6915a02a3f8c21d9079dd096bebcaab45daa3e
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-04-29 09:59:26 +0300

    use MUSIC for spotify instead of pmus

--- statusbar.sh
+++ statusbar.sh
@@ -22,23 +22,23 @@ MEM() {
     free -h | awk '/Mem/ {print $3 "/" $2}'
 }
 
-MUSIC() {
-    current_song=$(pmus -r current)
-    [ "$current_song" = "" ] && echo NO MUSIC && return
-    is_liked=$(pmus -r "is_liked $(echo $current_song | cut -d ' ' -f1)")
-    [ "$(pmus -r mode)" = "LOOP_SONG" ] && echo -n '🔂'
-    $is_liked && echo -n "💕"
-    echo -n '🤘 '
-    echo -n "$current_song" | cut -d ' ' -f2- | tr -d '\n'
-    echo -n ' '
-    pmus -r progress
-}
-
 #MUSIC() {
-#    song=$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | grep 'title' -A1 | tail -1 | tr -s ' ' | cut -d ' ' -f4- | sed 's/^"//; s/"$//')
-#    artist=$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | grep ':artist' -A2 | tail -1 | tr -s ' ' | cut -d ' ' -f3- | sed 's/^"//; s/"$//')
-#    echo $song - $artist
+#    current_song=$(pmus -r current)
+#    [ "$current_song" = "" ] && echo NO MUSIC && return
+#    is_liked=$(pmus -r "is_liked $(echo $current_song | cut -d ' ' -f1)")
+#    [ "$(pmus -r mode)" = "LOOP_SONG" ] && echo -n '🔂'
+#    $is_liked && echo -n "💕"
+#    echo -n '🤘 '
+#    echo -n "$current_song" | cut -d ' ' -f2- | tr -d '\n'
+#    echo -n ' '
+#    pmus -r progress
 #}
 
+MUSIC() {
+    song=$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | grep 'title' -A1 | tail -1 | tr -s ' ' | cut -d ' ' -f4- | sed 's/^"//; s/"$//')
+    artist=$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | grep ':artist' -A2 | tail -1 | tr -s ' ' | cut -d ' ' -f3- | sed 's/^"//; s/"$//')
+    echo $song - $artist
+}
+
 date=$(date "+%H:%M:%S (%a) %d/%m/%y")
-echo "$(LAYOUT)|$(MUSIC)|🐏 $(MEM)|🔊 $(VOL)|🕒 $date"
+echo "$(LAYOUT)|🤘 $(MUSIC)|🐏 $(MEM)|🔊 $(VOL)|🕒 $date"
