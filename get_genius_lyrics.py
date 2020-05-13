08057f48004cf5272501cf072b4be753beee321c
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-05-13 14:54:53 +0300

    improve handling of names and url

--- get_genius_lyrics.py
+++ get_genius_lyrics.py
@@ -4,15 +4,22 @@ import requests
 from bs4 import BeautifulSoup
 import sys
 
-artist_name = sys.argv[2].replace(' ', '-').capitalize()
-song_name   = sys.argv[1].replace(' ', '-')
- 
-def get_lyrics_tag(artist_name, song_name):
-    url = "https://genius.com/{}-{}-lyrics".format(artist_name, song_name)
-    soup = BeautifulSoup(requests.get(url).content.decode(), 'html.parser')
-    return soup.find_all("div", {"class": "lyrics"})[0]
+def get_lyrics(song_name, artist_name):
+    CHARS_TO_REMOVE = ".'?!"
+    for char_to_remove in CHARS_TO_REMOVE:
+        artist_name = artist_name.replace(char_to_remove, "").replace('/', '-')
+        song_name = song_name.replace(char_to_remove, "")
+    url = "https://genius.com/{}-{}-lyrics".format(
+            artist_name.replace('& ', 'and ').replace(' ', '-').capitalize(),
+            song_name.replace('& ', 'and ').replace(' ', '-'))
+    bs = BeautifulSoup(requests.get(url).content.decode(), 'html.parser')
+    lyrics = bs.find_all("div", {"class": "lyrics"})[0].find_all('p')[0].text
+    return lyrics
 
-try:
-    print(get_lyrics_tag(artist_name, song_name).find_all('p')[0].text)
-except:
-    pass
+if __name__ == '__main__':
+    try:
+        artist_name = sys.argv[2]
+        song_name   = sys.argv[1]
+        print(get_lyrics(song_name, artist_name))
+    except:
+        pass
