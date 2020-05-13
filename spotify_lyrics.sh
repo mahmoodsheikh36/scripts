7b851bd783ea0e77796cb4aa265783f8cf48c38f
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-05-13 14:58:45 +0300

    remove unnecessary async job

--- spotify_lyrics.sh
+++ spotify_lyrics.sh
@@ -3,7 +3,7 @@ song=$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /
 artist=$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | grep ':artist' -A2 | tail -1 | tr -s ' ' | cut -d ' ' -f3- | sed 's/^"//; s/"$//')
 lyrics_file=~/media/lyrics/"$song - $artist"
 [ -f "$lyrics_file" ] && notify-send "$(cat "$lyrics_file")" || (\
-    notify-send -t 5000 "fetching lyrics.." &\
+    notify-send -t 5000 "fetching lyrics.."\
     lyrics="$(get_genius_lyrics.py "$song" "$artist")";\
     [ ! -z "$lyrics" ] && echo "$lyrics" > "$lyrics_file" &&\
     notify-send -t 600000 "$lyrics"\
