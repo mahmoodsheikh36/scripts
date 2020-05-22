ffed94e934c020362718b584007562511ea6ac86
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-05-22 14:26:05 +0300

    use user-agent header to avoid getting blocked by Genius

--- get_genius_lyrics.py
+++ get_genius_lyrics.py
@@ -4,6 +4,8 @@ import requests
 from bs4 import BeautifulSoup
 import sys
 
+user_agent = 'Mozilla/5.0 (Linux; U; Android 2.2) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1'
+
 def get_lyrics(song_name, artist_name):
     CHARS_TO_REMOVE = ".'?!"
     for char_to_remove in CHARS_TO_REMOVE:
@@ -12,7 +14,8 @@ def get_lyrics(song_name, artist_name):
     url = "https://genius.com/{}-{}-lyrics".format(
             artist_name.replace('& ', 'and ').replace(' ', '-').capitalize(),
             song_name.replace('& ', 'and ').replace(' ', '-'))
-    bs = BeautifulSoup(requests.get(url).content.decode(), 'html.parser')
+    bs = BeautifulSoup(requests.get(url, headers={'user-agent': user_agent}
+        ).content.decode(), 'html.parser')
     lyrics = bs.find_all("div", {"class": "lyrics"})[0].find_all('p')[0].text
     return lyrics
 
