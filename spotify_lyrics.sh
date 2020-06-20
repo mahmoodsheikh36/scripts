8868ad112a2eaf6b5f68eced1603fb2d570b1d72
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-06-21 02:13:18 +0300

    use ~/data instead of ~/media

--- spotify_lyrics.sh
+++ spotify_lyrics.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh
 song=$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | grep 'title' -A1 | tail -1 | tr -s ' ' | cut -d ' ' -f4- | sed 's/^"//; s/"$//')
 artist=$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | grep ':artist' -A2 | tail -1 | tr -s ' ' | cut -d ' ' -f3- | sed 's/^"//; s/"$//')
-lyrics_file=~/media/lyrics/"$song - $artist"
+lyrics_file=~/data/lyrics/"$song - $artist"
 [ -f "$lyrics_file" ] && notify-send -t 6000000 "$(cat "$lyrics_file")" || (\
     notify-send -t 5000 "fetching lyrics..";\
     lyrics="$(get_genius_lyrics.py "$song" "$artist")";\
