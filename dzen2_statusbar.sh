883d029ec19a6e64fe780db8175955d99a1cc300
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-07-06 13:04:54 +0300

    make music function get data from spotify not pmus, fix statusbar

--- dzen2_statusbar.sh
+++ dzen2_statusbar.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh
 
 emoji_font="Noto Color Emoji-13"
-text_font="Source Code Pro-13"
+text_font="Hack-14"
 
 volume_emoji="^fn($emoji_font)🔊^fn($text_font)"
 time_emoji="^fn($emoji_font)🕒^fn($text_font)"
@@ -9,7 +9,7 @@ liked_song_emoji="^fn($emoji_font)💕^fn($text_font)"
 music_emoji="^fn($emoji_font)🤘^fn($text_font)"
 
 VOL() {
-    amixer get Master | awk '/%/ {print $4}' | tr -d '[]'
+    pactl list sinks | awk '/^\sVolume:/ {print $5}'
 }
 
 LAYOUT() {
@@ -17,34 +17,31 @@ LAYOUT() {
 }
 
 MUSIC() {
-    current_song=$(music_daemon_cmd.sh current)
-    is_liked=$(music_daemon_cmd.sh is_liked $(echo $current_song | cut -d ' ' -f1))
-    echo -n "$music_emoji "
-    $is_liked && echo -n "$liked_song_emoji $current_song" ||\
-        echo -n "$current_song"
-    echo -n ' '
-    music_daemon_cmd.sh progress
+    song=$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | grep 'title' -A1 | tail -1 | tr -s ' ' | cut -d ' ' -f4- | sed 's/^"//; s/"$//')
+    artist=$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | grep ':artist' -A2 | tail -1 | tr -s ' ' | cut -d ' ' -f3- | sed 's/^"//; s/"$//')
+    echo $song - $artist
 }
 
 WORKSPACES() {
     workspaces=$(bspc query --desktops -d .local --names)
     current_workspace=$(bspc query --desktops -d focused --names)
-    active_color="#ffffff"
     for workspace in $workspaces; do
         [ "$workspace" = "$current_workspace" ] &&\
-            echo -n "^fg(#00ff00)[$workspace]^fg(#bbbbbb)" || echo -n "$workspace"
+            echo -n "^fg(#00ff00)[$workspace]^fg(#ffffff)" || echo -n "$workspace"
         echo -n ' '
     done
 }
 
 (while true; do
     date=$(date "+%H:%M:%S (%a) %d/%m/%y")
-    echo "$(LAYOUT) | $(MUSIC) | $volume_emoji $(VOL) | $time_emoji $date"
+    echo "$(LAYOUT) | $music_emoji $(MUSIC) | $volume_emoji $(VOL) | $time_emoji $date"
     sleep 0.5;
 done) | dzen2 -p -x 5 -y 5 -w 1910 -fn "$text_font" -title-name dzen2_statusbar_top &
 
 (while true; do
     date=$(date "+%H:%M:%S (%a) %d/%m/%y")
     echo "$(WORKSPACES)"
-    sleep 0.5;
-done) | dzen2 -p -x 5 -y 5 -w 1910 -y 1050 -fn "$text_font" -title-name dzen2_statusbar_bottom &
+    sleep 0.35;
+done) | dzen2 -p -x 5 -y 5 -w 1910 -y 1050 -fn "$text_font" -title-name dzen2_statusbar_bottom
+
+wait
