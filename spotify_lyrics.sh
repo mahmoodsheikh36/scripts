d7877c5b69a98c80a8dee639a6b2effb5169b4bd
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-07-11 22:50:57 +0300

    replace / with -

--- spotify_lyrics.sh
+++ spotify_lyrics.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/sh
 song=$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | grep 'title' -A1 | tail -1 | tr -s ' ' | cut -d ' ' -f4- | sed 's/^"//; s/"$//')
 artist=$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | grep ':artist' -A2 | tail -1 | tr -s ' ' | cut -d ' ' -f3- | sed 's/^"//; s/"$//')
+
+song=$(echo $song | tr '/' '-')
+artist=$(echo $artist | tr '/' '-')
+
 lyrics_file=~/data/lyrics/"$song - $artist"
 [ -f "$lyrics_file" ] && notify-send -t 6000000 "$song - $artist" "\n$(cat "$lyrics_file" | cut -c1-75)" || (\
     notify-send -t 5000 "fetching lyrics..";\
