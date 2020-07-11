fe7b4350e50aae8116f81ad282935bb365cbab3b
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-07-11 22:51:15 +0300

    use -thread_queue_size 1024 to prevent errors

--- record.sh
+++ record.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/sh
 
-ffmpeg -f v4l2 -video_size 1280x720 -i /dev/video0 -f alsa -i default -c:v libx265 -crf 28 -c:a aac -b 300k ~/webcam/$(date | tr ' ' '_').mp4
+ffmpeg -f v4l2 -video_size 1280x720 -thread_queue_size 1024 -i /dev/video0 -f alsa -thread_queue_size 1024 -i default -c:v libx265 -crf 28 -c:a aac -b 300k ~/webcam/$(date | tr ' ' '_').mp4
