1447954ceb9ee6ed4c1e56710a83b7754063b6c0
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-07-27 01:57:52 +0300

    choose a random filename when saving remote image

--- open_url_image.sh
+++ open_url_image.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/sh
 
+mkdir /tmp/online_images/ 2>/dev/null
+
+random_filename=$(head -c 100 /dev/urandom | tr -dc A-Za-z0-9)
+
 url=$(echo -n "$1" | cut -d '?' -f1 | tr -d '\n')
-curl -L -o /tmp/image "$url"
-sxiv /tmp/image
+curl -L -o /tmp/online_images/$random_filename "$url"
+sxiv /tmp/online_images/$random_filename
