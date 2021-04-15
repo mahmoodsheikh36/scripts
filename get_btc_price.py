96f44466db11c3762ee4a883bb1f897118032f94
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2021-04-15 21:31:15 +0300

    script to get coin price

---
+++ get_btc_price.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import requests
+import json
+
+price_bought_at = 50000
+
+response = requests.get('https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD')
+data = json.loads(response.text)
+current_price = data["USD"]
+change_in_price = (data["USD"] - price_bought_at) / price_bought_at * 100
+print("{}$ {:.2f}%".format(current_price, change_in_price))
