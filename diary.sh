a611c07efdfbb3dd61c54fca2cc7cc19f560dd84
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2021-04-15 21:28:31 +0300

    change diary file locations

--- diary.sh
+++ diary.sh
@@ -6,9 +6,13 @@ notify-send "writing diaries for $date" -t 10000
 
 vim /tmp/diary
 
+save_dir="$HOME/data/diary"
+
+[ ! -d "$save_dir" ] && mkdir "$save_dir"
+
 if [ -f /tmp/diary ]; then
     if [ ! "`cat /tmp/diary`" = "" ]; then
-        diary_file="$HOME/media/diary/`echo $date | tr "/" "_"`"
+        diary_file="$save_dir/`echo $date | tr "/" "_"`.diary"
         echo "$date" > "$diary_file"
         cat /tmp/diary >> "$diary_file"
         echo `date "+%H:%M:%S %d/%m/%y" | tr "/" "_"` >> "$diary_file"
